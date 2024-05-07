88a63b9f328ef21e02972cc07b8677336d0891cb
fapfaff <fabian-pfaff@outlook.de>
2024-05-08 00:37:39 +0200

    feat: add default layout with bg-color

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-screen h-screen bg-amber-100">
+        <header>
+            <div class="w-full flex justify-between text-amber-800">
+                <div class="ps-4 pe-4 pt-1 pb-1">
+                    <button>Menu</button>
+                </div>
+                <div class="ps-4 pe-4 pt-1 pb-1">
+                    <button>Language</button>
+                </div>
+            </div>    
+        </header>
+        <!-- Output the page content  -->
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
