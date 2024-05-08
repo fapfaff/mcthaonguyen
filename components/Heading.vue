7c1893819f8a8f1a402b2669269d5609982f7029
fapfaff <fabian-pfaff@outlook.de>
2024-05-08 14:01:57 +0200

    feat: add Heading component

---
+++ components/Heading.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="text-4xl text-amber-800 text-center underline underline-offset-8 tracking-wider">
+        {{ text }}
+    </h1>
+</template>
+<script setup>
+    defineProps({
+        text: String
+    })
+</script>
