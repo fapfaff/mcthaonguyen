d97c46d2bf8e87dfa096b90020e56d14f0f7e80b
fapfaff <fabian-pfaff@outlook.de>
2024-05-09 21:02:57 +0200

    feat: add service card

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="border-2 border-soft-peach p-1">
-        <div class="bg-soft-peach w-full p-2 ps-5 pe-5">
+    <div class="border-2 border-soft-peach p-1 h-full w-full">
+        <div class="bg-soft-peach w-full h-full p-4 ps-5 pe-5">
             <slot />
         </div>
     </div>
---
+++ components/ServiceCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <Card>
+        <div class="flex flex-col items-center pt-3 w-100">
+            <img :src="src" class="w-10 h-10" :alt="`${title} Icon`">
+            <h1 class="text-3xl text-center pb-1 mt-3 mb-2"> {{ title }} </h1>
+            <p class="text-center"> {{ description }}</p>
+        </div>
+    </Card>
+</template>
+<script setup>
+const props = defineProps(['title', 'description', 'src']);
+</script>
+<style scoped>
+    h1 {
+        font-family: "Playfair Display";
+    }
+</style>
