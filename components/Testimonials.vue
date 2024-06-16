39d31108241cbca18e93dc55fb2bdc7b994b0433
fapfaff <fabian-pfaff@outlook.de>
2024-06-16 20:50:27 +0200

    feat: make testimonial swiper responsive

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -3,13 +3,25 @@
   :modules="[SwiperAutoplay, SwiperPagination]"
   :loop="true"
   :slides-per-view="1"
-  :space-between="50"
+  :space-between="20"
   :pagination="{ clickable: true, el: '.swiper-pagination'}"
   :autoplay="{ delay: 5000 }"
   :observer="true"
   :observeParents="true"
   :observeSlideChildren="true"
   :resizeObserver="true"
+  :centerInsufficientSlides="true"
+  :breakpoints="{
+    '640': {
+      slidesPerView: 1,
+    },
+    '768': {
+      slidesPerView: 2,
+    },
+    '1024': {
+      slidesPerView: 4,
+    },
+  }"
   >
     <SwiperSlide
       v-for="testimonial in data.values()"
@@ -21,30 +33,11 @@
     </SwiperSlide>
     <div class="swiper-pagination"></div>
   </Swiper>
-  <!-- <div class="flex flex-row">
-    <div class="lex-grow">
-        <TestimonialCard
-          :testimonial="testimonial"
-          :isExpanded="isExpanded"
-          :isClamped="isClamped"
-          @toggleExpanded="toggleExpanded"
-        >
-        </TestimonialCard>
-    </div>  
-  </div> -->
-  <!-- <div class="flex flex-row justify-center items-center space-x-1 pt-2">
-        <button v-for="(_, i) in data" :key="i" class="rounded-full border-2 border-creme-700"
-            :class="i == currentCardIndex ? 'bg-creme-700 h-5 w-5' : 'hover:h-5 hover:w-5 h-4 w-4'"
-            @click="switchToCard(i)"
-            >
-        </button>
-    </div> -->
 </template>
 <style>
 h1 {
   font-family: "Playfair Display";
 }
-
 .swiper-container {
     overflow: visible;
 }
