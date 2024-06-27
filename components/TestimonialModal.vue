1e9ff0c768c51663ae13948e0ebdedd5efa3e07b
fapfaff <fabian-pfaff@outlook.de>
2024-06-27 13:03:03 +0200

    fix: update modal layout

--- components/TestimonialModal.vue
+++ components/TestimonialModal.vue
@@ -1,30 +1,35 @@
 <template>
   <VueFinalModal
     class="flex justify-center items-center"
-    content-class="flex flex-col max-w-xl max-h-[80%] overflow-y-scroll mx-4 p-4 bg-creme-200 rounded-lg space-y-2"
+    content-class="flex flex-col max-w-xl max-h-[80%] overflow-hidden mx-4 p-4 ps-6 pe-6 bg-creme-200 rounded-lg space-y-2"
     overlay-transition="vfm-fade"
     content-transition="vfm-fade"
     :lockScroll="true"
   >
     <div v-if="testimonial">
-      <div v-if="testimonial.img" class="pb-5 flex justify-center">
-        <img
-          :src="`/testimonials/${testimonial.img}`"
-          class="w-100 object-cover aspect-video rounded-lg"
-        />
-      </div>
-      <p class="text-xl">
-        {{ testimonial.title }}
-      </p>
-      <div>
-        <ContentRendererMarkdown :value="testimonial" />
-      </div>
-      <div class="flex justify-center w-full">
-        <button class="rounded-full bg-green-600 tect-blue-950 mt-4" @click="emit('close')">
-          <p class="p-1 ps-4 pe-4">Close</p>
+      <div class="flex flex-row justify-between pb-5">
+        <p class="text-xl tracking-tight text-apricot-950">
+          {{ testimonial.title }}
+        </p>
+        <button
+          @click="$emit('close')"
+          class="text-creme-950 hover:text-creme-800 leading-none"
+        >
+          ✕
         </button>
       </div>
-    </div>  
+      <div class="overflow-y-scroll max-h-[70vh] relative">
+        <div v-if="testimonial.img" class="pb-3 flex justify-center">
+          <img
+            :src="`/testimonials/${testimonial.img}`"
+            class="w-100 object-cover aspect-video rounded-lg"
+          />
+        </div>
+        <div class="text-apricot-950">
+          <ContentRendererMarkdown :value="testimonial" />
+        </div>
+      </div>
+    </div>
   </VueFinalModal>
 </template>
 <script setup lang="ts">
@@ -32,10 +37,10 @@ import { VueFinalModal } from "vue-final-modal";
 import type { Testimonial } from "~/types/testimonial";
 
 defineProps<{
-    testimonial?: Testimonial,
-}>()
+  testimonial?: Testimonial;
+}>();
 
 const emit = defineEmits<{
-(e: 'close'): void
-}>()
+  (e: "close"): void;
+}>();
 </script>
