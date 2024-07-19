eaa9bae6d715088f733e773d8da0a9b22fe42d74
fapfaff <fabian-pfaff@outlook.de>
2024-07-19 21:02:15 +0200

    feat: add legal disclosure

--- pages/legalDisclosure.vue
+++ pages/legalDisclosure.vue
@@ -1,3 +1,14 @@
 <template>
-    <Heading :text="$t('route.legalDisclosure')"></Heading>
-</template>
+
+    <div class="px-12">
+      <Heading class="pt-2" :text="$t('route.legalDisclosure')"></Heading>
+      <ContentRendererMarkdown class="rendered" :value="data"/>
+    </div>
+  </template>
+  <script setup>
+  const { locale } = useI18n();
+  const { data } = await useAsyncData("legal", () =>
+    queryContent(`${locale.value}/etc/`).where({ key: 'legal'}).findOne()
+  );
+  </script>
+  
