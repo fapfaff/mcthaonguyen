f2a05b4710c09a677be8eeaa42f8fb28b1e2b414
fapfaff <fabian-pfaff@outlook.de>
2024-07-21 12:57:13 +0200

    fix: footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full bg-creme-200 0 py-10 flex justify-evenly">
-    <a href="/legalDisclosure" class="no-underline hover:text-creme-700 hover:font-semibold">>{{ $t('route.legalDisclosure') }}</a>
-    <a href="/privacyPolicy" class="no-underline hover:text-creme-700 hover:font-semibold">>{{ $t('route.privacyPolicy')}}</a>
+    <a href="/legalDisclosure" class="no-underline hover:text-creme-700 hover:font-semibold">{{"> " + $t('route.legalDisclosure') }}</a>
+    <a href="/privacyPolicy" class="no-underline hover:text-creme-700 hover:font-semibold">{{"> " + $t('route.privacyPolicy')}}</a>
   </div>
 </template>
