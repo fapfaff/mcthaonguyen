1979df7fcadf90444ef417b0dbdeef76b3ed4597
fapfaff <fabian-pfaff@outlook.de>
2024-10-04 16:01:29 +0200

    fix: consistent gap between service cards

--- components/Services.vue
+++ components/Services.vue
@@ -1,27 +1,13 @@
 <template>
 
   <div
-    class="flex flex-col flex-wrap justify-center items-stretch space-y-2 md:space-y-0 md:flex-row md:gap-x-5"
+    class="flex flex-col flex-wrap justify-center items-stretch gap-2 md:flex-row md:gap-5"
   >
   <div class="service-wrapper" v-for="service in data.values()">
       <ServiceCard
         :service="service"
       ></ServiceCard>
     </div>
-    <!-- <div class="service-wrapper" >
-      <ServiceCard
-        src="/icons/wedding_ring.webp"
-        :title="$t('services.marriages')"
-        description="Als Traurednerin mache ich eure freie Trauung unvergesslich und begleite euch den ganzen Abend mit moderierter Unterhaltung, Spielen und Programmpunkten – damit sich alle eure Gäste in die Feier eingebunden und wertgeschätzt fühlen."
-      ></ServiceCard>
-    </div>
-    <div class="service-wrapper">
-      <ServiceCard
-        src="/icons/cake.webp"
-        :title="$t('services.birthdays')"
-        description="Für deine Geburtstagsfeier sorge ich mit zweisprachiger Moderation dafür, dass alle Programmpunkte reibungslos ablaufen und jeder die Feier in vollen Zügen genießen kann."
-      ></ServiceCard>
-    </div> -->
   </div>
 </template>
 <style scoped>
