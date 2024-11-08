01bf9307188f1ca7ac0f18ab80fea60c6b886d87
fapfaff <fabian-pfaff@outlook.de>
2024-11-08 15:40:09 +0100

    fix: add text shadow to heading

--- components/Heading.vue
+++ components/Heading.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2 class="text-4xl text-creme-700 text-center underline underline-offset-8 tracking-wider pb-10 pt-16">
+    <h2 class="text-4xl text-creme-700 text-shadow-gold text-center underline underline-offset-8 tracking-wider pb-10 pt-16">
         {{ text }}
     </h2>
 </template>
@@ -9,7 +9,7 @@
     })
 </script>
 <style scoped>
-    h1 {
-        font-family: 'Playfair Display';
-    }
+    .text-shadow-gold {
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3); /* Adjust as needed */
+    }   
 </style>
